cd3a24289a984939cdcdb095037a5c53bb414677
Jonas Köhnen <jonas.koehnen@sap.com>
2025-05-26 18:42:36 +0200

    Introduce ExternalKey with PKCS#11 support
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- wrapper.go
+++ wrapper.go
@@ -1,10 +1,12 @@
 // Copyright (c) HashiCorp, Inc.
+// Copyright (c) 2025 OpenBao a Series of LF Projects, LLC
 // SPDX-License-Identifier: MPL-2.0
 
 package wrapping
 
 import (
 	"context"
+	"crypto"
 )
 
 type HmacComputer interface {
@@ -14,11 +16,11 @@ type HmacComputer interface {
 
 type InitFinalizer interface {
 	// Init allows performing any necessary setup calls before using a
-	// Wrapper.
+	// Wrapper or ExternalKey.
 	Init(ctx context.Context, options ...Option) error
 
-	// Finalize can be called when all usage of a Wrapper is done if any cleanup
-	// or finalization is required.
+	// Finalize can be called when all usage of a Wrapper or ExternalKey
+	// is done if any cleanup or finalization is required.
 	Finalize(ctx context.Context, options ...Option) error
 }
 
@@ -31,19 +33,18 @@ type Wrapper interface {
 	// KeyId is the ID of the key currently used for encryption
 	KeyId(context.Context) (string, error)
 
-	// SetConfig applies the given options to a wrapper and returns
+	// SetConfig applies the given options to a Wrapper and returns
 	// configuration information. WithConfigMap will almost certainly be
-	// required to be passed in to give wrapper-specific configuration
-	// information to the wrapper. WithKeyId is also supported.
+	// required to be passed in to provide wrapper-specific configuration.
 	SetConfig(ctx context.Context, options ...Option) (*WrapperConfig, error)
 
 	// Encrypt encrypts the given byte slice and stores the resulting
-	// information in the returned blob info. Which options are used depends on
-	// the underlying wrapper. Supported options: WithAad.
+	// information in the returned blob info. Which options are used
+	// depends on the underlying wrapper.
 	Encrypt(ctx context.Context, plaintext []byte, options ...Option) (*BlobInfo, error)
 	// Decrypt decrypts the given byte slice and stores the resulting
-	// information in the returned byte slice. Which options are used depends on
-	// the underlying wrapper. Supported options: WithAad.
+	// information in the returned byte slice. Which options are used
+	// depends on the underlying wrapper.
 	Decrypt(ctx context.Context, ciphertext *BlobInfo, options ...Option) ([]byte, error)
 }
 
@@ -52,4 +53,24 @@ type Wrapper interface {
 type KeyExporter interface {
 	// KeyBytes returns the "current" key bytes
 	KeyBytes(context.Context) ([]byte, error)
+}
+
+// ExternalKey is an interface where supporting implementations enable access
+// to cryptographic operations via the standard library's crypto.Signer and
+// crypto.Decrypter interfaces.
+type ExternalKey interface {
+	// SetConfig applies the given options to an ExternalKey.
+	// WithConfigMap will almost certainly be required to be passed in to
+	// provide wrapper-specific configuration. Supported options will be
+	// ones for general client configuration and not bound to a specific
+	// key. To access keys after setting the configuration, see GetKey.
+	SetConfig(ctx context.Context, options ...Option) error
+	// Signer retrieves a crypto.Signer.
+	// Supported options will let you bind to a specific key in the KMS,
+	// generic client-level configuration is passed in SetConfig.
+	Signer(ctx context.Context, options ...Option) (crypto.Signer, error)
+	// Decrypter retrieves a crypto.Decrypter.
+	// Supported options will let you bind to a specific key in the KMS,
+	// generic client-level configuration is passed in SetConfig.
+	Decrypter(ctx context.Context, options ...Option) (crypto.Decrypter, error)
 }
--- wrappers/pkcs11/options.go
+++ wrappers/pkcs11/options.go
@@ -1,184 +1,345 @@
-// Copyright (c) 2024 OpenBao a Series of LF Projects, LLC
 // Copyright (c) HashiCorp, Inc.
+// Copyright (c) 2025 OpenBao a Series of LF Projects, LLC
 // SPDX-License-Identifier: MPL-2.0
 
 package pkcs11
 
 import (
+	"fmt"
+	"strconv"
+
 	wrapping "github.com/openbao/go-kms-wrapping/v2"
+	"github.com/openbao/openbao/api/v2"
 )
 
-// getOpts iterates the inbound Options and returns a struct
-func getOpts(opt ...wrapping.Option) (*options, error) {
-	// First, separate out options into local and global
-	opts := getDefaultOptions()
-	var wrappingOptions []wrapping.Option
-	var localOptions []OptionFunc
-	for _, o := range opt {
+const (
+	EnvHsmWrapperLib         = "BAO_HSM_LIB"
+	EnvHsmWrapperSlot        = "BAO_HSM_SLOT"
+	EnvHsmWrapperTokenLabel  = "BAO_HSM_TOKEN_LABEL"
+	EnvHsmWrapperPin         = "BAO_HSM_PIN"
+	EnvHsmWrapperMaxParallel = "BAO_HSM_MAX_PARALLEL"
+	EnvHsmWrapperKeyId       = "BAO_HSM_KEY_ID"
+	EnvHsmWrapperKeyLabel    = "BAO_HSM_KEY_LABEL"
+	EnvHsmWrapperKeyType     = "BAO_HSM_KEY_TYPE"
+	EnvHsmWrapperMechanism   = "BAO_HSM_MECHANISM"
+	EnvHsmWrapperRsaOaepHash = "BAO_HSM_RSA_OAEP_HASH"
+)
+
+// clientOptions are the options relevant for client configuration.
+type clientOptions struct {
+	lib         string
+	slotNumber  *uint
+	tokenLabel  string
+	pin         string
+	maxSessions int
+}
+
+// keyOptions are the options relevant for key configuration.
+type keyOptions struct {
+	keyId     string
+	keyLabel  string
+	keyType   string
+	mechanism string
+	hash      string
+}
+
+// wrapperOptions are the options relevant for wrapper configuration.
+type wrapperOptions struct {
+	*clientOptions
+	*keyOptions
+}
+
+type (
+	// ClientOption is used to set client-related options.
+	ClientOption func(*clientOptions) error
+	// KeyOption is used to set key-related options.
+	KeyOption func(*keyOptions) error
+)
+
+// sortOpts sorts a list of wrapping.Option into three buckets.
+func sortOpts(opts []wrapping.Option) (*wrapping.Options, []ClientOption, []KeyOption, error) {
+	var globalOpts []wrapping.Option
+	var clientOpts []ClientOption
+	var keyOpts []KeyOption
+
+	for _, o := range opts {
 		if o == nil {
 			continue
 		}
 		iface := o()
 		switch to := iface.(type) {
 		case wrapping.OptionFunc:
-			wrappingOptions = append(wrappingOptions, o)
-		case OptionFunc:
-			localOptions = append(localOptions, to)
+			globalOpts = append(globalOpts, o)
+		case ClientOption:
+			clientOpts = append(clientOpts, to)
+		case KeyOption:
+			keyOpts = append(keyOpts, to)
 		}
 	}
 
-	// Parse the global options
-	var err error
-	opts.Options, err = wrapping.GetOpts(wrappingOptions...)
+	global, err := wrapping.GetOpts(globalOpts...)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return global, clientOpts, keyOpts, nil
+}
+
+// clientOptsFromConfigMap gets a clientOptions  from a config map.
+func clientOptsFromConfigMap(config map[string]string) (*clientOptions, error) {
+	var opts clientOptions
+	for key, val := range config {
+		switch key {
+		case "lib":
+			opts.lib = val
+		case "slot":
+			slot, err := ParseSlotNumber(val)
+			if err != nil {
+				return nil, err
+			}
+			opts.slotNumber = &slot
+		case "token_label":
+			opts.tokenLabel = val
+		case "pin":
+			opts.pin = val
+		case "max_parallel": // Called "max_parallel" for compability with upstream
+			sessions, err := strconv.ParseInt(val, 10, 32)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse max sessions value: %w", err)
+			}
+			opts.maxSessions = int(sessions)
+		}
+	}
+	return &opts, nil
+}
+
+// keyOptsFromConfigMap gets a keyOptions with from a config map.
+func keyOptsFromConfigMap(config map[string]string) (*keyOptions, error) {
+	var opts keyOptions
+	for key, val := range config {
+		switch key {
+		case "key_id":
+			opts.keyId = val
+		case "key_label":
+			opts.keyLabel = val
+		case "key_type":
+			opts.keyType = val
+		case "mechanism":
+			opts.mechanism = val
+		case "rsa_oaep_hash":
+			opts.hash = val
+		}
+	}
+	return &opts, nil
+}
+
+// getWrapperOpts evaluates options that apply to a Wrapper.
+// Environment variables are read unless disallowed via WithDisallowEnvVars.
+func getWrapperOpts(opts []wrapping.Option) (*wrapperOptions, error) {
+	globalOpts, clientOpts, keyOpts, err := sortOpts(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	// Don't ever return blank options
-	if opts.Options == nil {
-		opts.Options = new(wrapping.Options)
-	}
-
-	// Local options can be provided either via the WithConfigMap field
-	// (for over the plugin barrier or embedding) or via local option functions
-	// (for embedding). First pull from the option.
-	if opts.WithConfigMap != nil {
-		for k, v := range opts.WithConfigMap {
-			switch k {
-			// case "key_id", "kms_key_id": // deprecated backend-specific value, set global
-			case "key_id":
-				opts.withKeyId = v
-			case "slot":
-				opts.withSlot = v
-			case "pin":
-				opts.withPin = v
-			case "lib", "module":
-				opts.withLib = v
-			case "token", "token_label":
-				opts.withTokenLabel = v
-			case "label", "key_label":
-				opts.withKeyLabel = v
-			case "mechanism":
-				opts.withMechanism = v
-			case "rsa_oaep_hash":
-				opts.withRsaOaepHash = v
-			}
+	if !globalOpts.WithDisallowEnvVars {
+		if globalOpts.WithConfigMap == nil {
+			globalOpts.WithConfigMap = make(map[string]string)
 		}
+		mergeConfigMapWithEnv(globalOpts.WithConfigMap)
 	}
 
-	// Now run the local options functions. This may overwrite options set by
-	// the options above.
-	for _, o := range localOptions {
-		if o != nil {
-			if err := o(&opts); err != nil {
-				return nil, err
-			}
-		}
+	var options wrapperOptions
+	options.clientOptions, err = clientOptsFromConfigMap(globalOpts.WithConfigMap)
+	if err != nil {
+		return nil, err
 	}
-
-	if err := wrapping.ParsePaths(&opts.withPin); err != nil {
+	options.keyOptions, err = keyOptsFromConfigMap(globalOpts.WithConfigMap)
+	if err != nil {
 		return nil, err
 	}
 
-	return &opts, nil
+	for _, o := range clientOpts {
+		if err := o(options.clientOptions); err != nil {
+			return nil, err
+		}
+	}
+	for _, o := range keyOpts {
+		if err := o(options.keyOptions); err != nil {
+			return nil, err
+		}
+	}
+
+	return &options, nil
 }
 
-// OptionFunc holds a function with local options
-type OptionFunc func(*options) error
-
-// options = how options are represented
-type options struct {
-	*wrapping.Options
-
-	withSlot        string
-	withPin         string
-	withLib         string
-	withKeyId       string
-	withKeyLabel    string
-	withTokenLabel  string
-	withMechanism   string
-	withRsaOaepHash string
-}
-
-func getDefaultOptions() options {
-	return options{}
-}
-
-// WithSlot sets the slot
-func WithSlot(slot string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withSlot = slot
-			return nil
-		})
+func mergeConfigMapWithEnv(config map[string]string) {
+	if env := api.ReadBaoVariable(EnvHsmWrapperLib); env != "" {
+		config["lib"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperSlot); env != "" {
+		config["slot"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperTokenLabel); env != "" {
+		config["token_label"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperPin); env != "" {
+		config["pin"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperMaxParallel); env != "" {
+		config["max_parallel"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperKeyId); env != "" {
+		config["key_id"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperKeyLabel); env != "" {
+		config["key_label"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperKeyType); env != "" {
+		config["key_type"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperMechanism); env != "" {
+		config["mechanism"] = env
+	}
+	if env := api.ReadBaoVariable(EnvHsmWrapperRsaOaepHash); env != "" {
+		config["rsa_oaep_hash"] = env
 	}
 }
 
-// WithSlot sets the slot
-func WithTokenLabel(slot string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withTokenLabel = slot
-			return nil
-		})
+// getExternalKeyOpts evaluates options that apply to an ExternalKey.
+// Environment variables are never read.
+func getExternalKeyOpts(opts []wrapping.Option) (*clientOptions, error) {
+	globalOpts, clientOpts, _, err := sortOpts(opts)
+	if err != nil {
+		return nil, err
 	}
+	options, err := clientOptsFromConfigMap(globalOpts.WithConfigMap)
+	if err != nil {
+		return nil, err
+	}
+	for _, o := range clientOpts {
+		if err := o(options); err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
 }
 
-// WithPin sets the pin
-func WithPin(pin string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withPin = pin
-			return nil
-		})
+// getSignerDecrypterOpts evaluates options that apply
+// to signers/decrypters derived from an ExternalKey.
+// Environment variables are never read.
+func getSignerDecrypterOpts(opts []wrapping.Option) (*keyOptions, error) {
+	globalOpts, _, keyOpts, err := sortOpts(opts)
+	if err != nil {
+		return nil, err
 	}
+	options, err := keyOptsFromConfigMap(globalOpts.WithConfigMap)
+	if err != nil {
+		return nil, err
+	}
+	for _, o := range keyOpts {
+		if err := o(options); err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
 }
 
-// WithLib sets the module
+// WithLib sets the PKCS#11 module (dynamic library).
 func WithLib(lib string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withLib = lib
+	return func() any {
+		return ClientOption(func(o *clientOptions) error {
+			o.lib = lib
 			return nil
 		})
 	}
 }
 
-// WithLabel sets the label
-func WithKeyId(keyId string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withKeyId = keyId
+// WithSlot sets the token slot.
+func WithSlot(slot uint) wrapping.Option {
+	return func() any {
+		return ClientOption(func(o *clientOptions) error {
+			o.slotNumber = &slot
 			return nil
 		})
 	}
 }
 
-// WithLabel sets the label
+// WithTokenLabel sets the token label.
+func WithTokenLabel(label string) wrapping.Option {
+	return func() any {
+		return ClientOption(func(o *clientOptions) error {
+			o.tokenLabel = label
+			return nil
+		})
+	}
+}
+
+// WithPin sets the pin for the token slot.
+func WithPin(pin string) wrapping.Option {
+	return func() any {
+		return ClientOption(func(o *clientOptions) error {
+			o.pin = pin
+			return nil
+		})
+	}
+}
+
+// WithMaxSessions sets the maximum concurrent sessions against the the token slot.
+// Set to a value less than 1 to automatically choose the limit.
+func WithMaxSessions(sessions int) wrapping.Option {
+	return func() any {
+		return ClientOption(func(o *clientOptions) error {
+			o.maxSessions = sessions
+			return nil
+		})
+	}
+}
+
+// WithKeyId sets the key id.
+func WithKeyId(id string) wrapping.Option {
+	return func() any {
+		return KeyOption(func(o *keyOptions) error {
+			o.keyId = id
+			return nil
+		})
+	}
+}
+
+// WithKeyLabel sets the key label.
 func WithKeyLabel(label string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withKeyLabel = label
+	return func() any {
+		return KeyOption(func(o *keyOptions) error {
+			o.keyLabel = label
 			return nil
 		})
 	}
 }
 
-// WithMechanism sets the mechanism
+// WithKeyType sets the key type.
+func WithKeyType(ty string) wrapping.Option {
+	return func() any {
+		return KeyOption(func(o *keyOptions) error {
+			o.keyType = ty
+			return nil
+		})
+	}
+}
+
+// WithMechanism sets the key mechanism.
 func WithMechanism(mechanism string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withMechanism = mechanism
+	return func() any {
+		return KeyOption(func(o *keyOptions) error {
+			o.mechanism = mechanism
 			return nil
 		})
 	}
 }
 
-// WithRsaOaepHash sets the RSA OAEP Hash mechanism
-func WithRsaOaepHash(hashMechanisme string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withRsaOaepHash = hashMechanisme
+// WithRsaOaepHash sets the RSA-OAEP Hash mechanism.
+func WithRsaOaepHash(hash string) wrapping.Option {
+	return func() any {
+		return KeyOption(func(o *keyOptions) error {
+			o.hash = hash
 			return nil
 		})
 	}
