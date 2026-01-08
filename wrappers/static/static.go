371c3508ac9ccf2dd5c8b2b5908eaacc7cff4cc9
Jonas Köhnen <jonas.koehnen@sap.com>
2026-01-08 17:17:20 +0100

    wrappers/static: Fix backwards-compatibility with v2.4
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- wrappers/static/static.go
+++ wrappers/static/static.go
@@ -202,7 +202,10 @@ func (s *Wrapper) Encrypt(ctx context.Context, plaintext []byte, opts ...wrappin
 	ciphertext := gcm.Seal(nil, nil, plaintext, opt.WithAad)
 
 	ret := &wrapping.BlobInfo{
-		Ciphertext: ciphertext,
+		// 12 bytes = 96-bit nonce. Split these for backwards
+		// compatibility with OpenBao v2.4 and lower. Also see
+		// https://github.com/openbao/openbao/issues/2230.
+		Iv: ciphertext[:12], Ciphertext: ciphertext[12:],
 		KeyInfo: &wrapping.KeyInfo{
 			KeyId: s.currentKeyId,
 		},
@@ -214,20 +217,18 @@ func (s *Wrapper) Encrypt(ctx context.Context, plaintext []byte, opts ...wrappin
 // Decrypt is used to decrypt the ciphertext
 func (s *Wrapper) Decrypt(ctx context.Context, in *wrapping.BlobInfo, opts ...wrapping.Option) ([]byte, error) {
 	switch in.KeyInfo.KeyId {
+	case "":
+		return nil, errors.New("empty key id")
 	case s.previousKeyId:
-		if s.previousKeyId == "" {
-			return nil, fmt.Errorf("unknown key id for data: `%v`", in.KeyInfo.KeyId)
-		}
-
-		return s.decryptWithKey(ctx, in, s.previousKey, opts...)
+		return s.decryptWithKey(in, s.previousKey, opts...)
 	case s.currentKeyId:
-		return s.decryptWithKey(ctx, in, s.currentKey, opts...)
+		return s.decryptWithKey(in, s.currentKey, opts...)
 	default:
 		return nil, fmt.Errorf("unknown key id for data: `%v`", in.KeyInfo.KeyId)
 	}
 }
 
-func (s *Wrapper) decryptWithKey(ctx context.Context, in *wrapping.BlobInfo, key []byte, opts ...wrapping.Option) ([]byte, error) {
+func (s *Wrapper) decryptWithKey(in *wrapping.BlobInfo, key []byte, opts ...wrapping.Option) ([]byte, error) {
 	opt, err := wrapping.GetOpts(opts...)
 	if err != nil {
 		return nil, fmt.Errorf("failed parsing options: %w", err)
@@ -237,12 +238,19 @@ func (s *Wrapper) decryptWithKey(ctx context.Context, in *wrapping.BlobInfo, key
 	if err != nil {
 		return nil, fmt.Errorf("failed to create cipher: %w", err)
 	}
+
+	// OpenBao v2.5.0-beta20251125 would pass a combined nonce and
+	// ciphertext, which was backwards-incompatible with 2.4 and lower.
+	// Always combine them going forward for maximum compatibility.
+	// Also see https://github.com/openbao/openbao/issues/2230.
+	ciphertext := append(in.Iv, in.Ciphertext...)
+
 	gcm, err := cipher.NewGCMWithRandomNonce(block)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create GCM: %w", err)
 	}
 
-	plaintext, err := gcm.Open(nil, nil, in.Ciphertext, opt.WithAad)
+	plaintext, err := gcm.Open(nil, nil, ciphertext, opt.WithAad)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open ciphertext: %w", err)
 	}
