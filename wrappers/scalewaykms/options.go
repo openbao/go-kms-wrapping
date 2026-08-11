76ef76a75f37cb935aea8158a2e34a53e88e6476
Aubin Morand <aubin.mora@gmail.com>
2026-08-11 15:42:18 +0200

    fix(mr_comments): rework option pattern to conform to new standard in openbao
    skip reading files when WithDiallowEnvVars is set
    
    rework tests around require
    
    add test coverage for env config
    
    Signed-off-by: Aubin Morand <aubin.mora@gmail.com>

--- wrappers/scalewaykms/options.go
+++ wrappers/scalewaykms/options.go
@@ -7,80 +7,46 @@ import (
 	wrapping "github.com/openbao/go-kms-wrapping/v2"
 )
 
+func getDefaultOptions() options {
+	return options{}
+}
+
 // getOpts iterates the inbound Options and returns a struct
 func getOpts(opt ...wrapping.Option) (*options, error) {
-	// First, separate out options into local and global
 	opts := getDefaultOptions()
-	var wrappingOptions []wrapping.Option
-	var localOptions []OptionFunc
-	for _, o := range opt {
-		if o == nil {
-			continue
-		}
-		iface := o()
-		switch to := iface.(type) {
-		case wrapping.OptionFunc:
-			wrappingOptions = append(wrappingOptions, o)
-		case OptionFunc:
-			localOptions = append(localOptions, to)
-		}
-	}
 
-	// Parse the global options
 	var err error
-	opts.Options, err = wrapping.GetOpts(wrappingOptions...)
+	opts.Options, err = wrapping.GetOpts(opt...)
 	if err != nil {
 		return nil, err
 	}
 
-	// Don't ever return blank options
-	if opts.Options == nil {
-		opts.Options = new(wrapping.Options)
-	}
-
-	// Local options can be provided either via the WithConfigMap field
-	// (for over the plugin barrier or embedding) or via local option functions
-	// (for embedding). First pull from the option.
-	if opts.WithConfigMap != nil {
-		for k, v := range opts.WithConfigMap {
-			switch k {
-			case "key_id": // deprecated backend-specific value, set global
-				opts.WithKeyId = v
-			case "region":
-				opts.withRegion = v
-			case "project_id":
-				opts.withProjectID = v
-			case "access_key":
-				opts.withAccessKey = v
-			case "secret_key":
-				opts.withSecretKey = v
-			case "api_url":
-				opts.withAPIURL = v
-			}
+	for k, v := range opts.WithConfigMap {
+		switch k {
+		case "key_id": // deprecated backend-specific value, set global
+			opts.WithKeyId = v
+		case "region":
+			opts.withRegion = v
+		case "project_id":
+			opts.withProjectID = v
+		case "access_key":
+			opts.withAccessKey = v
+		case "secret_key":
+			opts.withSecretKey = v
+		case "api_url":
+			opts.withAPIURL = v
 		}
 	}
 
-	// Now run the local options functions. This may overwrite options set by
-	// the options above.
-	for _, o := range localOptions {
-		if o != nil {
-			if err := o(&opts); err != nil {
-				return nil, err
-			}
+	if !opts.WithDisallowEnvVars {
+		if err := wrapping.ParsePaths(&opts.withAccessKey, &opts.withSecretKey); err != nil {
+			return nil, err
 		}
-	}
-
-	if err := wrapping.ParsePaths(&opts.withAccessKey, &opts.withSecretKey); err != nil {
-		return nil, err
 	}
 
 	return &opts, nil
 }
 
-// OptionFunc holds a function with local options
-type OptionFunc func(*options) error
-
-// options = how options are represented
 type options struct {
 	*wrapping.Options
 
@@ -89,58 +55,4 @@ type options struct {
 	withAccessKey string
 	withSecretKey string
 	withAPIURL    string
-}
-
-func getDefaultOptions() options {
-	return options{}
-}
-
-// WithRegion provides a way to choose the region the key lives in
-func WithRegion(with string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withRegion = with
-			return nil
-		})
-	}
-}
-
-// WithProjectID provides a way to choose the project the key belongs to
-func WithProjectID(with string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withProjectID = with
-			return nil
-		})
-	}
-}
-
-// WithAccessKey provides a way to choose the access key
-func WithAccessKey(with string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withAccessKey = with
-			return nil
-		})
-	}
-}
-
-// WithSecretKey provides a way to choose the secret key
-func WithSecretKey(with string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withSecretKey = with
-			return nil
-		})
-	}
-}
-
-// WithAPIURL provides a way to choose the Scaleway API endpoint
-func WithAPIURL(with string) wrapping.Option {
-	return func() interface{} {
-		return OptionFunc(func(o *options) error {
-			o.withAPIURL = with
-			return nil
-		})
-	}
 }
